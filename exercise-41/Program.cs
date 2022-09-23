a24492900899ce4915f096fe312b550d0c85c8fa
Andrey <rolland81280@gmail.com>
2022-09-23 08:06:45 +0300

    сохран1

---
+++ exercise-41/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write($"Введи количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+int moreZero = 0;
+
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    if(massiveNumbers[i] > 0)
+    {
+      moreZero = moreZero + 1; 
+    }
+  }
+Console.Write("Количество чисел больше 0: " + moreZero);
+
